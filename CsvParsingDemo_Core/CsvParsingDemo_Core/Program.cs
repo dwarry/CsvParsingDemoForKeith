055f097bd0ff4dd6e3aa1742df926bf40ed4ebd3
Dave Warry <dave@warry.org.uk>
2017-06-01 03:36:34 +0100

    Initial set up of projects

---
+++ CsvParsingDemo_Core/CsvParsingDemo_Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsvParsingDemo_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ CsvParsingDemo_Core/CsvParsingDemo_Core_Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace CsvParsingDemo_Core_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
